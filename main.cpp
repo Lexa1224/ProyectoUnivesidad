45dd6e242311e80fc746f655796167f7b7c16a03
Lexa1224 <la17014@ues.edu.sv>
2023-06-07 21:50:51 -0600

    Se agrego una impresion

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int suma(int numero1, int numero2);
+
 int main(int argc, char** argv) {
 	cout<<"Hola Alex Cambio esta linea"<<endl;//Alex agrego esta linea para mostra github
 	//Fabio agrego este comentario
+	cout<<"Hola fui escrito en la maquina de Alex"<<endl;
+	
 	return 0;
 }
 
-su
+
