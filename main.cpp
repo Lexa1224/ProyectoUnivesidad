381a7f2802647fdcc738fea84940ad346859be11
Lexa1224 <la17014@ues.edu.sv>
2023-06-09 23:13:28 -0600

    Comienza Programacion para lectura y escritura en el fichero. ALEX

--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
 int main(int argc, char** argv) {
-	cout<<"Hola Alex Cambio esta linea"<<endl;//Alex agrego esta linea para mostra github
-	//Fabio agrego este comentario
-	cout<<"Hola fui escrito en la maquina de Alex"<<endl;
-	//francisco agrego comentario
-	// francisco modificando el codigo 
-	
-	//hola francisco y fabio
-	
+
 	return 0;
 }
 
